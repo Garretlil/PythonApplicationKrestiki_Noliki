f3b76cea9a853fe36d64ac6839b09bec5625084d
Даниил <@WIN-ENF4NR8ET31>
2022-02-21 14:31:06 +0300

    R

--- PythonApplicationKrestiki_Noliki/PythonApplicationKrestiki_Noliki.py
+++ PythonApplicationKrestiki_Noliki/PythonApplicationKrestiki_Noliki.py
@@ -1,15 +1,26 @@
+print ("Возврат хода - буква R")
 setka=int(input('введи размер желаемого поля: '))
-def proverka_na_mesto(x):#x - место, которое проверяется
-    return x in arraysetka
+ 
      
 def vvod_XO(bukva_,n):
-    x=int(input('куда поставить' + ' '+ bukva_ +'? '))
-
-    if not(x<=n**2 and x>0 and proverka_na_mesto(x) ):
-       print('введи допустимое значение')
-       vvod_XO(bukva_,n)       
-    else:      
-       arraysetka[x-1]=bukva_
+    x=input('следующий ход игрока' + ' '+ bukva_ +', выберите номер: ')
+    if x.upper()=='R' :
+        if  len(arraysteps)!=0:
+            stepp=arraysteps[len(arraysteps)-1]# последний ход
+            arraysetka[stepp-1]=stepp
+            arraysteps.pop(len(arraysteps)-1)
+    else:
+        x=int(x)
+        try:
+            if not(x<=n**2 and x>0 ):
+                raise BaseException("введи допустимое значение")
+            if not(x in arraysetka):
+                raise BaseException("поле занято")
+            arraysetka[x-1]=bukva_
+            arraysteps.append(x)
+        except BaseException as ve:
+          print(ve)
+          vvod_XO(bukva_,n)    
         
 
 def ResearchOfSize(current_element,maxlensymbol_):    
@@ -75,14 +86,19 @@ def win(n):
         
     return False
 
+def GetSymbol():    
+   return words[len(arraysteps)%2] 
+    
+
 arraysetka=[]
+arraysteps=[]
 for i in range(1,setka**2+1):
     arraysetka.append(i)
 ToConsoleArray(setka)
 CheiHod=False
 words = ("X", "O")
 while True:
-    vvod_XO(words[int(CheiHod)],setka)
+    vvod_XO(GetSymbol(),setka)
     CheiHod =not CheiHod 
     ToConsoleArray(setka)    
     if win(setka):
